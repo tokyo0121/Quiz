19418f1bf274d87661ba8bc5b178363baad4001c
tokyo0121 <saki.goodtime@gmail.com>
2021-10-09 23:12:29 +0900

    個人戦チーム戦

---
+++ Quiz/TeamExplainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TeamExplainViewController.swift
+//  Quiz
+//
+//  Created by Saki Igeta on 2021/10/09.
+//  Copyright © 2021 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class TeamExplainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Quiz/TeamMainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TeamMainViewController.swift
+//  Quiz
+//
+//  Created by Saki Igeta on 2021/10/09.
+//  Copyright © 2021 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class TeamMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
